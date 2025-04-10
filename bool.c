d46052bdfaaf5dae1cac1f2ff89d9499b5d3feee
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-04-10 17:34:02 +0600

    code

---
+++ bool.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool b = false;
+    printf("%d", b); // Will print 1 because `true` is represented as 1
+    return 0;
+}
+
---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main(){
+
+    char c = '0990755';
+    printf("%c", c);
+
+    return 0;
+}
+
